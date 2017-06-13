2010c091f2d27f02b9c4ed4e8bad4ce747114e04
Sean McGrath <Sean McGrath>
2017-06-14 00:32:53 +0100

    added views folder

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
-    console.log('Hello world');
-})
+    res.render('index');
+});
+
 app.listen(3000);
-console.log('Server is running on port 3000')
+console.log('Server is running on port 3000....')
